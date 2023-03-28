be57f4b5318c8df7d12fe42536a5c5014466cd77
Ali Taghizada <alitaqizade2@gmail.com>
2023-03-28 11:22:33 +0400

    added script

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+let inputToDo = document.getElementById("input-toDo");
+let btnToDo = document.getElementsByClassName("btn-to-do")[0];
+let toDo = document.getElementsByClassName("to-do")[0];
+let doing = document.getElementsByClassName("doing")[0];
+let btnDoing = document.getElementsByClassName("btn-doing")[0];
+let btnDone = document.getElementsByClassName("btn-done")[0];
+
+let addButton = document.getElementsByClassName("add-button")[0];
+addButton.addEventListener("click", function(){
+    btnToDo.innerText = inputToDo.value;
+})
+
+btnToDo.addEventListener("click", function(){
+    btnDoing.innerText = btnToDo.textContent;
+    btnToDo.remove();
+    toDo.innerHTML += '<button class="btn-to-do text-start my-2"></button>'
+})
+
+btnDoing.addEventListener("click", function(){
+    btnDone.innerText = btnDoing.textContent;
+    btnDoing.remove();
+    doing.innerHTML += '<button class="btn-doing text-start my-2"></button>'
+})
+
+btnDone.addEventListener("click", function(){
+    btnDone.remove();
+})

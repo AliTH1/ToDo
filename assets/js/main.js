effee12098b705d40183ff7df4be9655afeaf499
Ali Taghizada <alitaqizade2@gmail.com>
2023-03-28 16:24:22 +0400

    fixV2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,29 @@
 let inputToDo = document.getElementById("input-toDo");
-let btnToDo = document.getElementsByClassName("btn-to-do")[0];
+let btnToDo = document.getElementsByClassName("btn-to-do");
 let toDo = document.getElementsByClassName("to-do")[0];
 let doing = document.getElementsByClassName("doing")[0];
 let btnDoing = document.getElementsByClassName("btn-doing")[0];
 let btnDone = document.getElementsByClassName("btn-done")[0];
 
-let addButton = document.getElementsByClassName("add-button")[0];
-addButton.addEventListener("click", function(){
-    btnToDo.innerText = inputToDo.value;
-})
+let count = 0;
 
 inputToDo.addEventListener("keyup", function(event){
     if(event.key == "Enter"){
-        btnToDo.innerText = inputToDo.value;
+        btnToDo[count].innerText = inputToDo.value;
+        count++;
+        toDo.innerHTML += '<button class="btn-to-do text-start my-2"></button>'
     }
 })
 
-btnToDo.addEventListener("click", function(){
-    btnDoing.innerText = btnToDo.textContent;
-    btnToDo.remove();
+let addButton = document.getElementsByClassName("add-button")[0];
+addButton.addEventListener("click", function(){
+    btnToDo[count].innerText = inputToDo.value;
+    count++;
     toDo.innerHTML += '<button class="btn-to-do text-start my-2"></button>'
 })
 
-btnDoing.addEventListener("click", function(){
-    btnDone.innerText = btnDoing.textContent;
-    btnDoing.remove();
-    doing.innerHTML += '<button class="btn-doing text-start my-2"></button>'
-})
-
-btnDone.addEventListener("click", function(){
-    btnDone.remove();
+btnToDo.addEventListener("click", function(){
+    btnDoing.innerText = btnToDo[count].textContent;
+    btnToDo.remove();
+    toDo.innerHTML += '<button class="btn-to-do text-start my-2"></button>'
 })
